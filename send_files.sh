aea1a907ce6467364f0207fa40bc1a26a71bb79e
Jack Tyler <tyler.jackoliver@gmail.com>
2019-05-21 22:45:15 +0100

    Completes Cray compatability

--- send_files.sh
+++ send_files.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-rsync -azP *.f90 jtyler@login.archer.ac.uk:/work/y14/y14/jtyler/MPI/
+rsync -azP case_study/ jtyler@login.archer.ac.uk:/work/y14/y14/jtyler/MPI_coursework/
 
 echo -e "Done."
